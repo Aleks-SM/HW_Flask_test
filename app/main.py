07f2c62373f093e9706e0322bf86101e22ad79e7
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-12-14 07:00:34 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask('app')
 
+@app.before_request
+def before_request():
+    session = Session()
+    request.session = session
+
+
+@app.after_request
+def after_request(response: flask.Response):
+    request.session.close()
+    return response
+
+
+@app.errorhandler(HttpError)
+def error_handler(error):
+    response = jsonify({"error": error.description})
+    response.status_code = error.status_code
+    return response
+
+
+user_view = UserView.as_view("user_view")
+
+app.add_url_rule("/users/<int:user_id>", view_func=user_view, methods=["GET", "PATCH", "DELETE"])
+app.add_url_rule("/users", view_func=user_view, methods=["POST"])
+app.add_url_rule("/login", view_func=LoginView.as_view('login_view'), methods=["POST"])
 
 if __name__ == '__main__':
   app.run()
