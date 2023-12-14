42b8361dce6c1f0bbde1068a540f80bc3a727665
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-12-14 06:59:39 +0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
-PG_USER
-PG_PASSWORD
-PG_HOST
-POG_PORT
-PG_DB
+POSTGRES_USER = os.getenv("POSTGRES_PASSWORD", "secret")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_USER", "user")
+POSTGRES_HOST = os.getenv("POSTGRES_DB", "avito_db")
+POSTGRES_PORT = os.getenv("POSTGRES_HOST", "127.0.0.1")
+POSTGRES_DB = os.getenv("POSTGRES_PORT", "5432")
