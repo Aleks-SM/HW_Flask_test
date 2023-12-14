9398ee74203a2b62212ca7db93a90dc0ba9c7525
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-12-14 07:34:49 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,57 @@
+import datetime
+from sqlalchemy import String, DateTime, func, ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from config import POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST
 
+
+PG_DSN = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = create_engine(PG_DSN)
+Session = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+  
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
+    registration_time: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+  
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    
+    @property
+    def dict(self):
+        return {
+            'user_id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'registration_time': self.registration_time.isoformat()
+        }
+
+
+class Adverts(Base):
+    __tablename__ = "adverts"
+  
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(20), nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+    date_post: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete='CASCADE'), nullable=False)
+
+    @property
+    def dict(self):
+        return {
+            'adv_id': self.id,
+            'description': self.description,
+            'date_post': self.date_post.isoformat()
+        }
+
+
+Base.metadata.create_all(bind=engine)
